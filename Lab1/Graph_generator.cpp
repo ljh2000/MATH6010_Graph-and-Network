837629cc1c71d0508d3e152a7566e5b975bb1933
ljh2000 <1197993966@qq.com>
2022-03-20 12:50:06 +0800

    Randomly generate graphs

--- Lab1/Graph_generator.cpp
+++ Lab1/Graph_generator.cpp
@@ -8,11 +8,36 @@
 #include <vector>
 #include <set>
 #include <queue>
+#include <ctime>
 
 using namespace std;
 
+const int maxn = 1000;
+int n,m;
+bool graph[maxn][maxn];
+
 int main()
 {
+    freopen("Graph.in","w",stdout);
+    n = 8;
+    m = 18;
+    cout << n << " " << m << endl;
 
+    srand(time(NULL));
+
+    int mcnt = 0;
+    while(mcnt < m) {
+        int x,y;
+        x = rand() % n + 1;
+        y = rand() % n + 1;
+        if(x > y) swap(x,y);
+
+        if(x == y || graph[x][y]) continue;
+
+        graph[x][y] = 1;
+        mcnt ++;
+        cout << x << " " << y << endl;
+    }
+    
     return 0;
 }
