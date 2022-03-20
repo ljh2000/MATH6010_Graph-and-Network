7900a535c4e8689e6b5ea8db5f38973a3b29ec9a
ljh2000 <1197993966@qq.com>
2022-03-20 12:37:28 +0800

    Lab1 initial commit

---
+++ Lab1/Graph_generator.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <cmath>
+#include <cstdlib>
+#include <vector>
+#include <set>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+
+    return 0;
+}
