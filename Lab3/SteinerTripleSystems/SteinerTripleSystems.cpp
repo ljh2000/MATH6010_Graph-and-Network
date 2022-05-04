9001a5de22bc5e2a00447707581b9768c06e81db
ljh2000 <1197993966@qq.com>
2022-05-05 02:18:21 +0800

    Create SteinerTripleSystems.cpp

---
+++ Lab3/SteinerTripleSystems/SteinerTripleSystems.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <cmath>
+#include <cstdlib>
+#include <vector>
+#include <set>
+#include <queue>
+#include <ctime>
+using namespace std;
+int v , NumBlocks;
+
+int main(int argc,char *argv[])
+//int main(int argc,char *argv[])
+{
+    /*
+    if (argc == 1){
+        n = 200;
+    } else {
+        n = atoi(argv[1]);
+    }*/
+
+    v = 10;
+    
+
+    return 0;
+}
